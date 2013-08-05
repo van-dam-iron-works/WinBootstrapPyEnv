51fe5d082c8891d39b84850c67c3144ae9701133
Ben Rousch <brousch@gmail.com>
2013-08-05 14:40:40 -0400

    Switched from wget to curl. Added support for automatically installing Python and PyWin32. Added Python3.3.

--- bootstrap27.bat
+++
@@ -1,5 +0,0 @@
-wget\wget.exe http://python-distribute.org/distribute_setup.py
-C:\Python27\python.exe distribute_setup.py
-del distribute*
-C:\Python27\Scripts\easy_install.exe pip
-C:\Python27\Scripts\pip.exe install virtualenv
--- bootstrap32.bat
+++
@@ -1,5 +0,0 @@
-wget\wget.exe http://python-distribute.org/distribute_setup.py
-C:\Python32\python.exe distribute_setup.py
-del distribute*
-C:\Python32\Scripts\easy_install.exe pip
-C:\Python32\Scripts\pip.exe install virtualenv
